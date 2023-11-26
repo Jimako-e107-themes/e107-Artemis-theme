c01d3c45fc2c4ad1a789c2faa4724a8c14c1b62b
Jimako <janamicek@gmail.com>
2023-11-26 12:37:21 +0100

    #4 use welcome message only

--- artemis/theme.php
+++ artemis/theme.php
@@ -171,38 +171,10 @@ if(!defined('e107_INIT'))
 			{
 
 				case 'wmessage':
-                
-					echo '<header class="header-2">
-						<div class="page-header min-vh-75 relative" style="background-image: url('.THEME.'soft-ui-design-system/img/curved-images/curved1.jpg)">
-						<span class="mask bg-gradient-info"></span>
-						<div class="container">
-							<div class="row">
-							<div class="col-lg-7 text-center mx-auto">';
-							if (!empty($caption)) {
-								echo '<h1 class="text-white pt-3 mt-n5" >'.$caption.'</h1>';
-							}          
-							echo '<p class="lead text-white mt-3">'.$text.'</p>';
-							echo '</div>
-									</div>
-								</div>
-								<div class="position-absolute w-100 z-index-1 bottom-0">
-									<svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 40" preserveAspectRatio="none" shape-rendering="auto">
-									<defs>
-										<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-									</defs>
-									<g class="moving-waves">
-										<use xlink:href="#gentle-wave" x="48" y="-1" fill="rgba(255,255,255,0.40" />
-										<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.35)" />
-										<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.25)" />
-										<use xlink:href="#gentle-wave" x="48" y="8" fill="rgba(255,255,255,0.20)" />
-										<use xlink:href="#gentle-wave" x="48" y="13" fill="rgba(255,255,255,0.15)" />
-										<use xlink:href="#gentle-wave" x="48" y="16" fill="rgba(255,255,255,0.95" />
-									</g>
-									</svg>
-								</div>
-								</div>
-							</header>'; 
- 
+					if (!empty($caption)) {
+						echo '<h1 class="text-white pt-3 mt-n5" >'.$caption.'</h1>';
+					}          
+					echo '<p class="lead text-white mt-3">'.$text.'</p>';
 	            break;
 
 				case 'actionbox':
@@ -226,6 +198,11 @@ if(!defined('e107_INIT'))
 					echo $text;
 					break;
 
+				case 'section':
+					echo '<div class="container">';
+					echo $text;
+					echo '</div>';
+					break;
 
 				case 'menu':
 					echo '<div class=" mb-4">';
